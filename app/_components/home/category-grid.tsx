5b0d3f7b60dfb3f47f483dc9342b32c47ac262f2
Nabil Afia <nabil.afia.afia@gmail.com>
2026-09-06 12:00:32 +0100

    feat: Implement categories section

--- app/_components/home/category-grid.tsx
+++ app/_components/home/category-grid.tsx
@@ -1,3 +1,61 @@
+import Link from "next/link";
+const categories = [
+  {
+    title: "web design",
+    href: "/web-design",
+    bgClassName: "bg-[url('/assets/home/mobile/image-web-design.jpg')]",
+  },
+  {
+    title: "app design",
+    href: "/app-design",
+    bgClassName: "bg-[url('/assets/home/mobile/image-app-design.jpg')]",
+  },
+  {
+    title: "graphic design",
+    href: "/graphic-design",
+    bgClassName: "bg-[url('/assets/home/mobile/image-graphic-design.jpg')]",
+  },
+];
+
 export default function CategoryGrid() {
-  return <h1>our categories</h1>;
+  return (
+    <section aria-labelledby="categories-heading" className="px-6 my-20">
+      <h2 id="categories-heading" className="sr-only">
+        Our Design Services
+      </h2>
+
+      <ul className="flex flex-col gap-6">
+        {categories.map((category) => (
+          <li key={category.href}>
+            <Link
+              href={category.href}
+              className={`group relative flex flex-col items-center justify-center min-h-[250px] 
+rounded-2xl p-6 text-center text-white transition-all duration-300 overflow-hidden bg-cover 
+bg-center ${category.bgClassName}`}
+            >
+              <div
+                className="absolute inset-0 bg-black/50 group-hover:bg-peach/80 transition-colors duration-300"
+                aria-hidden="true"
+              />
+
+              <div className="relative  flex flex-col items-center gap-3">
+                <h3 className="text-[28px] leading-[36px] font-medium tracking-[1.4px] uppercase">
+                  {category.title}
+                </h3>
+                <p className="flex items-center gap-4 text-h3 tracking-[5px] uppercase font-medium">
+                  View Projects
+                  <span
+                    aria-hidden="true"
+                    className="text-peach group-hover:translate-x-1 transition-transform"
+                  >
+                    &gt;
+                  </span>
+                </p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
--- app/_components/home/hero.tsx
+++ app/_components/home/hero.tsx
@@ -1,3 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
 export default function Hero() {
-  return <h1>hero</h1>;
+  return (
+    <section
+      className="pbs-10 px-3 text-center gap-3 flex flex-col justify-center items-center bg-peach bg-[url('/assets/home/desktop/bg-pattern-hero-home.svg')] 
+      bg-no-repeat bg-position-[left_center] overflow-hidden"
+    >
+      <h1 className="text-preset-3 text-white ">
+        Award-winning custom designs and digital branding solutions
+      </h1>
+      <p className="text-body text-white">
+        With over 10 years in the industry, we are experienced in creating fully
+        responsive websites, app design, and engaging brand experiences. Find
+        out more about our services.
+      </p>
+      <Link
+        href="/about"
+        className="bg-white text-dark-grey text-h3 px-6 py-4 rounded-lg font-medium uppercase 
+        tracking-widest transition-colors duration-200 hover:bg-light-peach hover:text-white 
+        active:bg-light-peach active:text-white focus-visible:outline-2 focus-visible:outline-offset-2 
+        focus-visible:outline-white"
+      >
+        LEARN MORE
+      </Link>
+      <div className="relative w-full max-w-[327px] mx-auto -mt-18 overflow-hidden pointer-events-none">
+        <Image
+          src="/assets/home/desktop/image-hero-phone.png"
+          alt=""
+          aria-hidden="true"
+          width={624}
+          height={913}
+          priority
+          className="w-[140%] max-w-none h-auto -translate-x-[14.28%] object-cover object-top -mb-[75%]"
+        />
+      </div>
+    </section>
+  );
 }
